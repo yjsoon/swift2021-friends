575bc256750248880312aeefc98539f9f4add27b
YJ Soon <yjsoon@tinkertanker.com>
2021-06-26 11:06:21 +0800

    Added a Friend struct

---
+++ Friends List/Friend.swift
@@ -0,0 +1,19 @@
+//
+//  Friend.swift
+//  Friends List
+//
+//  Created by Yin Jie Soon on 26/6/21.
+//
+
+import Foundation
+
+struct Friend: Identifiable {
+    
+    var id = UUID()
+    
+    var name: String
+    var icon: String
+    var school: String
+    var slothImage: String
+    
+}
