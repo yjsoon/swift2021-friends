d813e4a8f19003e22329b4a90009abd435c1b8ce
YJ Soon <yjsoon@tinkertanker.com>
2021-06-26 10:38:37 +0800

    Initial Commit

---
+++ Friends List/Friends_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friends_ListApp.swift
+//  Friends List
+//
+//  Created by Yin Jie Soon on 26/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Friends_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
